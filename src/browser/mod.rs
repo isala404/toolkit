15d11bc707ddf69483f3bfc2b5703cea45ca9a75
Isala Piyarisi <mail@isala.me>
2023-10-22 12:10:43 +0530

    Restructured the code base

--- src/browser/mod.rs
+++ src/browser/mod.rs
@@ -1,12 +1,14 @@
 use thirtyfour::{DesiredCapabilities, WebDriver};
 use tokio::sync::Mutex;
+use crate::utils;
 
 mod handler;
 mod model;
 
 pub async fn selenium() -> (handler::Selenium, WebDriver) {
+
     let caps = DesiredCapabilities::chrome();
-    let web_driver = match WebDriver::new("http://localhost:9515", caps).await {
+    let web_driver = match WebDriver::new(utils::CHROME_DRIVER_ENDPOINT.as_str(), caps).await {
         Ok(d) => d,
         Err(e) => {
             panic!("Failed to create session: {:?}", e)
